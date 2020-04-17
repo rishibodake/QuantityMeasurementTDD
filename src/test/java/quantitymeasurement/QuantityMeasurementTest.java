d15ecb9e3dc4cc3dee6777493284216ed5a77b24
Hrishikesh <rishibodake@gmail.com>
2020-04-18 00:02:35 +0530

    Added Test For Same Quantity But Different Values as 0 and 1

--- src/test/java/quantitymeasurement/QuantityMeasurementTest.java
+++ src/test/java/quantitymeasurement/QuantityMeasurementTest.java
@@ -113,4 +113,12 @@ public class QuantityMeasurementTest {
         Length value2InYards = new Length(AllUnitsToUse.UnitConverter.YARDS,0.0);
         Assert.assertEquals(value1InYards,value2InYards);
     }
+
+    //Test For Equal Quantity but Different values
+    @Test
+    public void givenValuesIn0yardAnd1yardValues_ShouldReturnFalse() {
+        Length value1InYards = new Length(AllUnitsToUse.UnitConverter.YARDS,0.0);
+        Length value2InYards = new Length(AllUnitsToUse.UnitConverter.YARDS,1.0);
+        Assert.assertNotEquals(value1InYards, value2InYards);
+    }
 }
