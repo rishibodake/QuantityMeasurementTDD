dce5394932e78976995a7b9c29990a05cc871eab
Hrishikesh <rishibodake@gmail.com>
2020-04-17 16:45:55 +0530

    Modified The Name Of Test Case

--- src/test/java/quantitymeasurement/QuantityMeasurementTest.java
+++ src/test/java/quantitymeasurement/QuantityMeasurementTest.java
@@ -5,10 +5,13 @@ import org.junit.Test;
 
 public class QuantityMeasurementTest {
 
+    //TestCase 1
     @Test
-    public void given0FeetAnd0Feet_whenCompare_shouldReturnTrue(){
+    public void given0FeetAnd0Feet_whenCompare_shouldReturnEqual(){
       Feet feetValue1 = new Feet(0.0);
       Feet feetValue2 = new Feet(0.0);
       Assert.assertEquals(feetValue1,feetValue2);
     }
+
+
 }
