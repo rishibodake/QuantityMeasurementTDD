c9b979874e6a122bc7603e3f9a8ba42c265df562
Hrishikesh <rishibodake@gmail.com>
2020-04-17 16:35:31 +0530

    Added:Overrided equals Method To Pass The Test

--- src/main/java/quantitymeasurement/Feet.java
+++ src/main/java/quantitymeasurement/Feet.java
@@ -1,9 +1,19 @@
 package quantitymeasurement;
 
+import java.util.Objects;
+
 public class Feet {
     private final double valueOfFeet;
 
     public Feet(double valueOfFeet) {
         this.valueOfFeet = valueOfFeet;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Feet feet = (Feet) o;
+        return Double.compare(feet.valueOfFeet, valueOfFeet) == 0;
     }
 }
