c8bdadf0031a65eb31b95f1892aca66caac0981c
rishibodake <rishibodake@gmail.com>
2020-04-21 23:44:22 +0530

    Added:Test For 0Cm and 0Cm should return true

--- src/main/java/quantitymeasurement/AllUnitsToUse.java
+++ src/main/java/quantitymeasurement/AllUnitsToUse.java
@@ -16,9 +16,13 @@ public class AllUnitsToUse {
             public double convertToInches(double length) {
                 return length * 3 * 12;
             }
+        },
+        CENTIMETER {
+            public double convertToInches(double length) {
+                return length;
+            }
+
         };
-
-
         public double convertToInches(double length) {
             return 0.0;
         }
